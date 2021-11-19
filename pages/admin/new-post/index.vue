5e038244a01dd522aaeb8a6ce5839edcbe028f26
Fernando Silva <fernandosilvadj@hotmail.com>
2021-11-19 11:50:11 -0300

    feat: load posts from firebase

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm @submit="onSubimetted"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+import axios from 'axios'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  methods: {
+    onSubimetted(postData) {
+      axios.post('https://nuxt-app-ebdeb-default-rtdb.firebaseio.com/posts.json', postData)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .new-post-form {
+    width: 500px;
+  }
+}
+</style>
+
---
+++ store/index.js
@@ -0,0 +1,66 @@
+import Vuex from "vuex";
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      }
+    },
+    actions: {
+      async nuxtServerInit(vuexContext, context) {
+        try {
+          const postData = await this.$axios.$get(
+            "https://nuxt-app-ebdeb-default-rtdb.firebaseio.com/posts.json"
+          );
+          const postsArray = [];
+
+          for (const key in postData) {
+            if (postData.hasOwnProperty(key)) {
+              postsArray.push({ ...postData[key], id: key });
+            }
+          }
+
+          vuexContext.commit("setPosts", postsArray);
+        } catch (error) {
+          context.error(error);
+        }
+
+        // return new Promise((resolve, reject) => {
+        //   setTimeout(() => {
+        //     vuexContext.commit("setPosts", [
+        //       {
+        //         id: "1",
+        //         title: "First Post",
+        //         previewText: "This is our first post!",
+        //         thumbnail:
+        //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        //       },
+        //       {
+        //         id: "2",
+        //         title: "Second Post",
+        //         previewText: "This is our second post!",
+        //         thumbnail:
+        //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        //       }
+        //     ]);
+        //     resolve();
+        //   }, 1000);
+        // });
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  });
+};
+
+export default createStore;
